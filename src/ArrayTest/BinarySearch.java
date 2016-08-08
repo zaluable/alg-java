59aa5a04dd55bcb4581b5644a35f8d0303570a86
zaluable <zaluable@gmail.com>
2016-08-08 23:27:25 +0800

    调整格式 tab换成space

--- src/ArrayTest/BinarySearch.java
+++ src/ArrayTest/BinarySearch.java
@@ -5,22 +5,27 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class BinarySearch {
-	public static int rank(int key,int[] a){
+	public static int rank(int key, int[] a) {
 		int lo = 0;
-		int hi = a.length-1;
-		
-		while (lo <= hi){
+		int hi = a.length - 1;
+
+		while (lo <= hi) {
 			int mid = lo + (hi - lo) / 2;
-			if (key > a[mid]) lo = mid + 1;
-			else if (key < a[mid]) hi = mid - 1;
-			else 	return a[mid];
+			if (key > a[mid])
+				lo = mid + 1;
+			else if (key < a[mid])
+				hi = mid - 1;
+			else
+				return a[mid];
 		}
+
 		return -1;
 	}
-@Test
-//unit test
+
+	@Test
+	// unit test
 	public void testRank() {
-		int[] a = {2,3,4,5,6};
+		int[] a = { 2, 3, 4, 5, 6 };
 		int key = 5;
 		Arrays.sort(a);
 		System.out.println(rank(key, a));
