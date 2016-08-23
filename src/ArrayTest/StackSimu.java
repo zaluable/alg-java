676eeac5326349c2247a872312d61b4b1ee26cf2
zaluable <zaluable@gmail.com>
2016-08-23 18:12:54 +0800

    模拟stack实现，实现iterable接口实现迭代器逆序访问

---
+++ src/ArrayTest/StackSimu.java
@@ -0,0 +1,112 @@
+package ArrayTest;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author zhangxiao3
+ * 模拟栈的构成
+ * 实现Iterable接口，实现Iteraor方法返回一个可以反向输出的迭代器
+ */
+public class StackSimu<Item> implements Iterable<Item>{
+	//定义一个数组变量
+	private Item[] stack;
+	//定义一个指针一方面标示数组的下标，一方面在赋值后N++标示数组size
+	private int N = 0 ;
+	//初始化stack容量
+	@SuppressWarnings("unchecked")
+	public StackSimu(int cap){
+		stack = (Item[])new Object[cap];
+	}
+	
+	public StackSimu(){
+	}
+	
+	public Boolean isEmpty(){
+		return N == 0;
+	}
+	public int size(){
+		return N;
+	}
+	//指针向前指一位
+	public Item pop(){
+		Item tempItem = stack[--N];
+		//避免游离对象
+		stack[N] = null;
+		//如果有效元素的长度等于栈长度的1/4，栈长度减半
+		if (N > 0 && N == stack.length/4){resize(stack.length/2);}
+		return tempItem;
+	}
+	//将s赋值给当前指针位置，同时指针指向下一位
+	public void push(Item item){
+		//容量不够时数组长度加倍
+		if (N == stack.length){resize(stack.length*2);}
+		stack[N++] = item;
+	}
+	private Item[] resize(int max){
+		@SuppressWarnings("unchecked")
+		Item[] temp = (Item[])new Object[max];
+		for(int i = 0;i < N;i++){
+			temp[i] = stack[i] ;
+		}
+		stack = temp;
+		return stack;
+	}
+	//覆盖toString方法
+	public String toString(){
+		@SuppressWarnings("unchecked")
+		Item[] c = (Item[])new Object[N];
+		for(int j = 0; j < N;j++){
+			c[j] = stack[j];
+		}
+		return Arrays.toString(c);
+	}
+	//实现Iterator方法返回一个逆向迭代器
+	public Iterator<Item> iterator(){
+		return new ReverseArray();
+	}
+	
+	private class ReverseArray implements Iterator<Item>{
+		private int i = N;
+		@Override
+		public boolean hasNext() {
+			if(i > 0){
+			return true;
+			}else return false;
+		}
+
+		@Override
+		public Item next() {
+			return stack[--i];
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	@Test
+	public void testStackSimu(){
+//	public static void main(String[] args){
+		String[] strlist = {"to","be","or","not","to","-","be","-","-","that","-","-","-","is"};
+		StackSimu<String> stackSimu = new StackSimu<String>(5);
+		for (int i = 0; i < strlist.length; i++){
+			String str = strlist[i];
+			if (str.equals("-")){
+				stackSimu.pop();
+			}else{
+				stackSimu.push(str);
+			}			
+			System.out.println("Size:"+stackSimu.size());
+		}
+		Iterator<String> iter = stackSimu.iterator();
+		while (iter.hasNext()){
+			String s = iter.next();
+			System.out.println(s);
+		}
+	}
+}
---
+++ src/ArrayTest/UniqFact.java
@@ -0,0 +1,49 @@
+package ArrayTest;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class UniqFact {
+	//不使用ArrayList的way1
+	public int[] uniqFactWay1(int[] arr) {
+		//声明一个size变量指示uniq元素的size
+		int size = 0;
+		//循环arr数组内元素
+		for (int i = 0; i < arr.length; i++) {
+			//判断如果当前索引的元素是否与该元素之前的元素相等
+			//若相等，直接循环下一个元素
+			if (isDul(arr, i)){
+				continue;
+			}
+			//若不相等，将当前元素赋给size索引所在的位置，赋值后size自增1
+			arr[size++] = arr[i];
+		}
+		//循环完毕所有arr内元素后new一个新数组，将size索引前的数一一赋值给新数组
+		return subArr(arr, size);
+	}
+	private Boolean isDul(int[] arr,int index){
+		for(int i = 0; i < index; i++){
+			if (arr[index] == arr[i]) return true;
+		}
+		return false;
+	}
+	private int[] subArr(int[] arr,int size){
+		int[] newArr = new int[size];
+		for(int i = 0;i < size ; i++){
+			newArr[i] = arr[i];
+		}
+		return newArr;	
+	}
+	//test way1
+	@Test
+	public void testWay1(){
+//		int[] arr = {2, 2, 1, 2, 6, 6, 8, 8, 8, 11, 12};
+		int[] arr = {2, 2, 1, 2};
+		System.out.println("Before Uniq:"+Arrays.toString(arr));
+		int[] newArr = uniqFactWay1(arr);
+		System.out.println("After Uniq:"+Arrays.toString(newArr));
+	}
+	
+	
+}
